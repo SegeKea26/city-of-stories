d540f933283cad8c9f2a9ac093f2b43b2a916183
SegeKea26 <keano.segers@student.arteveldehs.be>
2026-01-07 23:36:14 +0100

    updated experience with camera, fog, extra restrictions, warmer light sources

--- src/Experience/Experience.jsx
+++ src/Experience/Experience.jsx
@@ -5,21 +5,23 @@ import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
 
 const Experience = () => {
   return (
-    <Canvas shadows>
-        <PerspectiveCamera makeDefault position={[0, 50, 100]} fov={50} />
+    <Canvas shadows className="canvas">
+        <fog attach="fog" args={['#1a1520', 50, 200]} />
+        
+        <PerspectiveCamera makeDefault position={[0, 40, 80]} fov={50} />
         <OrbitControls  
           enablePan={false} 
           enableZoom={false} 
           enableRotate={true}
           minAzimuthAngle={-Infinity}
           maxAzimuthAngle={Infinity}
-          minPolarAngle={0}
+          minPolarAngle={Math.PI / 2.5}
           maxPolarAngle={Math.PI / 2.5}
           target={[0, 0, 0]}
         />
         
-        <directionalLight position={[10, 20, 10]} intensity={1} />
-        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 20, 10]} intensity={1.2} color="#ffe0cc" />
+        <ambientLight intensity={0.5} color="#fff5f0" />
         
         <Winterfest />
       </Canvas>
