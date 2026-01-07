2f465024dc19bd548d0e11e3d43e2b329c65adfd
SegeKea26 <keano.segers@student.arteveldehs.be>
2026-01-07 23:34:41 +0100

    created footer component x initial styling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <p className='footer__text'>City of stories - Ghent Winterfest - by Segers Keano</p>
+    </footer>
+  )
+}
+
+export default Footer
