392bf97383063bf08f4feee1780875413d418e58
SegeKea26 <keano.segers@student.arteveldehs.be>
2026-01-07 16:00:32 +0100

    created Poi component

---
+++ src/components/POI/POI.jsx
@@ -0,0 +1,14 @@
+import { Html } from '@react-three/drei'
+import './POI.css'
+
+export function POI({ position, onClick }) {
+  return (
+    <group position={position}>
+      <Html>
+        <div className="poi" onClick={onClick}>
+          <div className="poi-dot" />
+        </div>
+      </Html>
+    </group>
+  )
+}
